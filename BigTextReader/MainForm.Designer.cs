21aaae0845035be709f1025a4e31457ad7946310
Tapas Nayak <tnk02.05@gmail.com>
2015-10-14 16:50:33 +0530

    adding files

---
+++ BigTextReader/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BigTextReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtxtboxContent = new System.Windows.Forms.RichTextBox();
+            this.loadNextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(634, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadNextToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // rtxtboxContent
+            // 
+            this.rtxtboxContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtboxContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtboxContent.Location = new System.Drawing.Point(0, 24);
+            this.rtxtboxContent.Name = "rtxtboxContent";
+            this.rtxtboxContent.Size = new System.Drawing.Size(634, 438);
+            this.rtxtboxContent.TabIndex = 1;
+            this.rtxtboxContent.Text = "";
+            this.rtxtboxContent.WordWrap = false;
+            // 
+            // loadNextToolStripMenuItem
+            // 
+            this.loadNextToolStripMenuItem.Name = "loadNextToolStripMenuItem";
+            this.loadNextToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadNextToolStripMenuItem.Text = "Load Next";
+            this.loadNextToolStripMenuItem.Click += new System.EventHandler(this.loadNextToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 462);
+            this.Controls.Add(this.rtxtboxContent);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(650, 500);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Big Text Reader";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtxtboxContent;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadNextToolStripMenuItem;
+    }
+}
+
---
+++ BigTextReader/MainForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace BigTextReader
+{
+    public partial class MainForm : Form
+    {
+        private List<string> currentLines;
+        private StreamReader textReader;
+        private int maxLines = 100;
+        private string currentFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            currentLines = new List<string>();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.RestoreDirectory = true;
+            ofd.Title = "Select a text file";
+            ofd.CheckFileExists = true;
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                currentLines.Clear();
+                currentFile = ofd.FileName;
+                textReader = new StreamReader(currentFile);
+                string line = textReader.ReadLine();
+                int lineCount = 0;
+                while (line != null && lineCount < maxLines)
+                {
+                    currentLines.Add(line);
+                    line = textReader.ReadLine();
+                    lineCount++;
+                }
+                LoadCurrentLines();
+            }
+        }
+
+        private void LoadCurrentLines()
+        {
+            string content = string.Empty;
+            foreach (string line in currentLines)
+            {
+                content += line + Environment.NewLine;
+            }
+            rtxtboxContent.Text = content;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string outputFileName = Interaction.InputBox("Enter File Name", "Input Dialog", "");
+            DirectoryInfo dinfo = Directory.GetParent(currentFile);
+            outputFileName = Path.Combine(dinfo.FullName, outputFileName);
+            StreamWriter sw = new StreamWriter(outputFileName);
+            for (int i = 0; i < currentLines.Count(); i++)
+            {
+                sw.WriteLine(currentLines[i]);
+            }
+            sw.Close();
+        }
+
+        private void loadNextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentLines.Clear();
+            string line = textReader.ReadLine();
+            int lineCount = 0;
+            while (line != null && lineCount < maxLines)
+            {
+                currentLines.Add(line);
+                line = textReader.ReadLine();
+                lineCount++;
+            }
+            LoadCurrentLines();
+        }
+    }
+}
